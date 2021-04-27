43332cbbe9a1aac0642682c3d0d40525e28b7cf9
RealChen <34754551+IronChen93@users.noreply.github.com>
2021-04-27 15:19:08 -0500

    Update cyberf32.py

--- cyberf32.py
+++ cyberf32.py
@@ -1,6 +1,8 @@
 # imports
 from instapy import InstaPy
 from instapy import smart_run
+import schedule
+import time
 
 # login credentials
 insta_username = 'cyber_f32'
@@ -9,43 +11,50 @@ insta_password = 'Tczcm1993,./'
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
+
+
 session = InstaPy(username=insta_username,
                   password=insta_password,
                   headless_browser=True)
 
 
 with smart_run(session):
+""" Activity flow """		
 
-  """ Activity flow """		
-
-  # general settings		
-  session.set_dont_include(["friend1", "friend2", "friend3"])		
-
-
-  # activity		
-  session.like_by_tags(['bmw', 'n55', '435i', 'bmw435i', 'bmwf32', 'bimmer', 'bmw4series', 'bmwf30', 'bmwnation', 'bmwusa'], amount=10)
-  session.set_dont_like(['nsfw', 'naked', 'nude', 'hotgirl', '🔞'])
-  session.set_do_follow(True, percentage=70)
-  session.set_do_comment(enabled=True, percentage=70)
-
-  # Joining Engagement Pods
-  
-  session.set_comments(comments)
-
-  comments = ['Nice shot🔥!',
-        'You whip is on 🔥🔥🔥',
-        'Your feeds are awesome 👍👌',
-        'Just incredible!! 🔥🤩',
-        'Your shot is on 🔥👌',
-        'Love your posts!!',
-        'Looks awesome!! 👌',
-        'This is really dope!! 👌',
-        ':raised_hands: Yes!']
+      # general settings		
+      session.set_dont_include(["friend1", "friend2", "friend3"])		
 
 
+      # activity		
+      session.like_by_tags(['bmw', 'n55', '435i', 'bmw435i', 'bmwf32', 'bimmer', 'bmw4series', 'bmwf30', 'bmwnation', 'bmwusa'], amount=10)
+      session.set_dont_like(['nsfw', 'naked', 'nude', 'hotgirl', '🔞', 'kia'])
+      session.set_do_follow(True, percentage=70)
+      session.set_do_comment(enabled=True, percentage=70)
 
-  #*******************
-  #Extra smart features
-  #*******************
-  
-  session.set_quota_supervisor(enabled=True, peak_comments_daily=300, peak_commnets_hourly=50)
+      # Joining Engagement Pods
+
+      session.set_comments(comments)
+
+      comments = ['Nice shot🔥!',
+            'You whip is on 🔥🔥🔥',
+            'Your feeds are awesome 👍👌',
+            'Just incredible!! 🔥🤩',
+            'Your shot is on 🔥👌',
+            'Love your posts!!',
+            'Looks awesome!! 👌',
+            'This is really dope!! 👌',
+            ':raised_hands: Yes!']
+
+      # Filter the users that you want to follow based on their follower & following status
+      session.set_relationship_bounds(enabled=True,
+                                          delimit_by_numbers=True,
+                                          max_followers=50000,
+                                          min_followers=50,
+                                          min_following=10)
+
+      #*******************
+      #Extra smart features
+      #*******************
+
+      # session.set_quota_supervisor
+            
